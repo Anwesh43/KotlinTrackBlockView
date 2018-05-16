a01500771b90987bd7b9f4941e343559f16d0275
Anwesh43 <anweshthecool0@gmail.com>
2018-05-17 05:00:55 +0530

    adding View in Activity

--- app/src/main/java/com/example/anweshmishra/kotlintrackblockview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlintrackblockview/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.anweshmishra.kotlintrackblockview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.trackblockview.TrackBlockView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        TrackBlockView.create(this)
 
     }
 }
