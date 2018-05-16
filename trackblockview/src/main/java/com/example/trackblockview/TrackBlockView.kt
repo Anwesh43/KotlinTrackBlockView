95ab373d35bd03277be0ff63923a3f9dad2612f0
Anwesh43 <anweshthecool0@gmail.com>
2018-05-17 04:19:48 +0530

    adding TrackBlockView class

--- trackblockview/src/main/java/com/example/trackblockview/TrackBlockView.kt
+++ trackblockview/src/main/java/com/example/trackblockview/TrackBlockView.kt
@@ -9,3 +9,18 @@ import android.content.Context
 import android.graphics.*
 import android.view.MotionEvent
 
+class TrackBlockView (ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
