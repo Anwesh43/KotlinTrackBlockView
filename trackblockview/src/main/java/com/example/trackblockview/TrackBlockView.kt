1477118977192d1807a454e9452371ffd6227465
Anwesh43 <anweshthecool0@gmail.com>
2018-05-17 04:16:59 +0530

    adding TrackBlockView module

---
+++ trackblockview/src/main/java/com/example/trackblockview/TrackBlockView.kt
@@ -0,0 +1,11 @@
+package com.example.trackblockview
+
+/**
+ * Created by anweshmishra on 17/05/18.
+ */
+
+import android.view.View
+import android.content.Context
+import android.graphics.*
+import android.view.MotionEvent
+
